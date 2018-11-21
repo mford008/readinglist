a96672eff7e5a73b172db0e5de8b41edff1bc3c6
Maddy Ford <mrhf008@gmail.com>
2018-11-20 19:15:38 -0800

    added book model

--- readinglist/settings.py
+++ readinglist/settings.py
@@ -6,8 +6,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 
 
 DEBUG = True
 
